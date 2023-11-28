2a4dde85bb698facd806e60d4dbb56b893ae4d49
Pizza <kasperpizza25@gmail.com>
2023-11-28 16:43:45 +0100

    Added tile grid

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ import pygame, sys
 WINDOW_SIZE = (800, 600)
 FPS = 60
 
+# grid is 40 by 30
+TILE_SIZE = 20
+GRID_X = WINDOW_SIZE[0] / 40
+GRID_Y = WINDOW_SIZE[1] / 30
+
 pygame.init()
 window = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("Magelock")
 clock = pygame.time.Clock()
+
+def getcoords(x, y):
+    return x * GRID_X, y * GRID_Y
 
 def main():
     while True:
@@ -15,8 +23,9 @@ def main():
                 pygame.quit()
                 sys.exit()
         
-        window.fill((100, 100, 200))
+        
         pygame.display.flip()
+        window.fill((100, 100, 200))
         clock.tick(FPS)
 
 
