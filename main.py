6e0eeaf73a4df6504cfdaeb6025c6a6a1db3fb73
Pizza <kasperpizza25@gmail.com>
2023-11-28 16:18:32 +0100

    Creates pygame display

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-import pygame
+import pygame, sys
+
+WINDOW_SIZE = (800, 600)
+FPS = 60
+
+pygame.init()
+window = pygame.display.set_mode(WINDOW_SIZE)
+pygame.display.set_caption("Magelock")
+clock = pygame.time.Clock()
 
 def main():
-    pass
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        window.fill((100, 100, 200))
+        pygame.display.flip()
+        clock.tick(FPS)
 
-main()
+
+if __name__ == "__main__":
+    main()
