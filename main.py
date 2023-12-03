bda6d6954120496d730a2fb99f203b04fe3f1321
Pizza <kasperpizza25@gmail.com>
2023-12-03 15:37:49 +0100

    Everything is broken

--- main.py
+++ main.py
@@ -19,10 +19,14 @@ clock = pygame.time.Clock()
 def getcoords(x, y):
     return x * GRID_X, y * GRID_Y
 
+# screen coords to world coords
+def getworldcoords(x, y):
+    return x / GRID_X, y / GRID_Y
+
 # mutable variables
 stage_hitbox = pygame.Rect(getcoords(2, 11)[0], getcoords(2, 11)[1], TILE_SIZE * 16, TILE_SIZE * 2)
 
-hitboxes = [stage_hitbox]
+hitboxes = [stage_hitbox,pygame.Rect(getcoords(2, 7)[0], getcoords(2, 7)[1], TILE_SIZE * 4, TILE_SIZE),pygame.Rect(getcoords(15, 10)[0], getcoords(15, 10)[1], TILE_SIZE * 4, TILE_SIZE)]
 
 showhitboxes = False
 
@@ -40,8 +44,9 @@ class Player(pygame.sprite.Sprite):
         self.image = classes[cls]
         self.imageleft = self.image
         self.imageright = pygame.transform.flip(self.image, True, False)
-        self.x = posx
-        self.y = posy
+        self.tempx = 0
+
+        self.rect = pygame.Rect(getcoords(posx, posy)[0], getcoords(posx, posy)[1], TILE_SIZE, TILE_SIZE)
 
         self.gravity = 2
         self.speed = 15
@@ -49,51 +54,83 @@ class Player(pygame.sprite.Sprite):
 
         self.isgrounded = False
 
-        self.xvel = 0
         self.yvel = 0
+        self.xvel = 0
 
+    # draws player
     def draw(self):
-        window.blit(self.image, (getcoords(self.x, self.y)))
+        window.blit(self.image, (self.rect.x, self.rect.y))
         if showhitboxes:
             pygame.draw.rect(window, (0,255,0), self.rect)
-            pygame.draw.rect(window, (255,0,0), self.groundrect)
 
     def update(self):
-        self.rect = pygame.Rect(getcoords(self.x, self.y)[0], getcoords(self.x, self.y)[1], TILE_SIZE, TILE_SIZE - 6)
-        self.groundrect = pygame.Rect(getcoords(self.x, self.y)[0] + 7.5, getcoords(self.x, self.y)[1] + TILE_SIZE - TILE_SIZE/5, TILE_SIZE - 15, TILE_SIZE - 30)
+        # checks for input and puts it in a list
+        intent = self.inputhandler()
 
+        # checks if we can move the player horizontally
+        self.yvel += self.gravity / 100
 
-        self.yvel += self.gravity
-        for i in hitboxes:
-            if self.groundrect.colliderect(i):
-                self.yvel = 0
-                self.isgrounded = True
-            else:
-                self.isgrounded = False
+        collideh = self.horizontal_collision(intent)
 
-        self.inputhandler()
+        #check vertical collision
+        self.vertical_collision()
 
-        for i in hitboxes:
-            if self.rect.colliderect(stage_hitbox):
-                self.xvel = 0
+        if intent[1] == 1:
+            self.yvel -= self.jumpvel / 100
+            self.isgrounded = False
+        
 
-        self.y += self.yvel / 100
-        self.x += self.xvel / 100
+        self.rect.y += getcoords(0,self.yvel)[1]
+        if collideh:
+            self.rect.x += getcoords(self.xvel,0)[0]
 
         self.xvel = 0
+    
+    def vertical_collision(self):
+        for i in hitboxes:
+            if self.rect.colliderect(i):
+                if self.yvel > 0:
+                    self.rect.bottom = i.top + 2
+                    self.isgrounded = True
+                if self.yvel < 0:
+                    self.rect.top = i.bottom
+                
+                self.yvel = 0
+
+    def horizontal_collision(self, intent):
+        if intent[0] == -1:
+            self.xvel = -self.speed / 100
+        if intent[0] == 1:
+            self.xvel = self.speed / 100
+
+        self.rect.x += getcoords(self.xvel,0)[0]
+        for i in hitboxes:
+            if self.rect.colliderect(i):
+                self.rect.x -= getcoords(self.xvel,0)[0]
+                self.xvel = 0
+                return False
+            
+        self.rect.x -= getcoords(self.xvel,0)[0]
+        self.xvel = 0
+        return True
+        
+            
+
 
     def inputhandler(self):
+        intent = [0,0]
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            self.xvel += -self.speed
-            self.image = self.imageleft
+            intent[0] = -1
         if keys[pygame.K_RIGHT]:
-            self.xvel += self.speed
-            self.image = self.imageright
+            intent[0] = 1
 
         if keys[pygame.K_UP] and self.isgrounded is True: 
-            self.yvel -= self.jumpvel
             self.isgrounded = False
+            intent[1] = 1
+
+        return intent
+    
 
 # creating player (temp)
 plr = Player("mage", 10, 7)
@@ -102,7 +139,7 @@ plr = Player("mage", 10, 7)
 # main function
 def main():
     while True:
-        
+
         # global events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
